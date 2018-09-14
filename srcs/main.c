ac5533ad4eb3cb2a1d6b1c535f02076ff331b806
Issac Smith <issmith@e1z3r7p25.42.us.org>
2018-09-14 15:26:28 -0700

    fix

--- srcs/main.c
+++ srcs/main.c
@@ -3,7 +3,7 @@
 int	main()
 {
 	char *str = "this is a string\n";
-	ft_printf("%p%s", &str, str);
-	printf("%p%s", &str, str);
+	ft_putnbr(ft_printf("%p%s", &str, str));
+	ft_putnbr(printf("%p%s", &str, str));
 	return (0);
 }
